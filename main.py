846d7fa01ca053be490faaaa53542358164cc74e
JordanKisiel <jordan.kisiel@gmail.com>
2024-09-28 01:10:31 -0400

    added player display

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +23,10 @@ def main():
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        player.draw(screen)
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
